a0f7bacfe029c8b1850a1b60fa770a2717ea24fb
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-03-17 19:17:06 -0400

    Fixed the get requests

--- myProjectVtAceIt/src/main/java/com/vtaceit/myProjectVtAceIt/controller/GuideController.java
+++ myProjectVtAceIt/src/main/java/com/vtaceit/myProjectVtAceIt/controller/GuideController.java
@@ -5,6 +5,9 @@ import com.vtaceit.myProjectVtAceIt.service.GuideService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 
 @RequestMapping()
@@ -16,13 +19,17 @@ public class GuideController {
     }
 
     @GetMapping(path="allGuides")
-    public void showAll(){
+    public List<Guide> getAll(){
+        return service.getAll();
 
     }
-    @GetMapping(path="{identifier}")
-    public void showByIdentifier(){
+    @GetMapping(path="getGuides/{identifier}")
+    public Optional<List<Guide>> getByIdentifier(@PathVariable("identifier") String identifier){
+        return service.getByIdentifier(identifier);
 
     }
+    //done
+    //add functionality which checks if there is already a dupe in the db
     @PostMapping(path="addGuide")
     public void addGuide(@RequestBody Guide guide){
         service.addGuide(guide);
--- myProjectVtAceIt/src/main/java/com/vtaceit/myProjectVtAceIt/repository/GuideRepo.java
+++ myProjectVtAceIt/src/main/java/com/vtaceit/myProjectVtAceIt/repository/GuideRepo.java
@@ -2,6 +2,14 @@ package com.vtaceit.myProjectVtAceIt.repository;
 
 import com.vtaceit.myProjectVtAceIt.model.Guide;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface GuideRepo extends JpaRepository<Guide, Long> {
+
+    //selecting from the Model class, not through database name
+    @Query("SELECT g from Guide g where g.courseIdentifier=?1")
+    Optional<List<Guide>> getByIdentifier(String identifier);
 }

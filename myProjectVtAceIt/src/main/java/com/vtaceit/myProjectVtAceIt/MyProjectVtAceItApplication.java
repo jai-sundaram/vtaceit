8718abed5cae3cf9a0d709a9b618e4f0e0042e2f
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-03-17 12:37:30 -0400

    Added the folder

---
+++ myProjectVtAceIt/src/main/java/com/vtaceit/myProjectVtAceIt/MyProjectVtAceItApplication.java
@@ -0,0 +1,13 @@
+package com.vtaceit.myProjectVtAceIt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProjectVtAceItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProjectVtAceItApplication.class, args);
+	}
+
+}
---
+++ myProjectVtAceIt/src/test/java/com/vtaceit/myProjectVtAceIt/MyProjectVtAceItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtaceit.myProjectVtAceIt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyProjectVtAceItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

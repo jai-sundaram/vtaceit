72e1c94b60ed486f4f17f9c09318cec5ea91dc77
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-03-24 15:48:45 -0400

    got started

---
+++ vtaceit-frontend/src/assets/Guide.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Guide = () => {
+    return (
+        <div className="border-2 w-200     bg-vtorange">
+            <div>
+                <h2 className = "text-white float-left">General Chemistry 1 </h2>
+                <h2 className= "text-white float-left"> Date Posted: 8/20/2024</h2>
+            </div>
+            <br />
+            <h2 className = "text-white text-left">Professor: Arachiage</h2>
+            <p>random random random random random</p>
+        </div>
+    )
+}
+export default Guide;

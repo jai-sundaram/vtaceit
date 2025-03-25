688012f032269b508a82b103e9f7c19aa19a89bc
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-03-24 20:41:05 -0400

    created guide template

--- vtaceit-frontend/src/assets/Guide.jsx
+++ vtaceit-frontend/src/assets/Guide.jsx
@@ -2,15 +2,28 @@ import React from 'react'
 
 const Guide = () => {
     return (
-        <div className="border-2 w-200     bg-vtorange">
-            <div>
-                <h2 className = "text-white float-left">General Chemistry 1 </h2>
-                <h2 className= "text-white float-left"> Date Posted: 8/20/2024</h2>
+        <div className="border-2 w-250 h-100 bg-vtorange ">
+            <h1 className = "mt-5 mr-219 mt-">DIFFICULTY</h1>
+
+            <div className="mt-1">
+                <h2 className = "text-white float-left ml-35  text-lg">Professor: Juste</h2>
+                <h2 className= "text-white float-right mr-6 text-lg "> Date Posted: 8/20/2024</h2>
             </div>
-            <br />
-            <h2 className = "text-white text-left">Professor: Arachiage</h2>
-            <p>random random random random random</p>
+            <div className="text-5xl text-center ml-5 border-1 border-black w-20 h-20 bg-gray-500 ">4</div>
+            <div className = "">
+                <h2 className = "text-white float-left ml-35 mt-1">Grade: B+</h2>
+                <h2 className = "text-white float-left ml-40 mt-1">Semester: Spring 2024</h2>
+                <h2 className = "text-white float-left ml-40 mt-1">Attendance: Mandatory</h2>
+            </div>
+            <h2 className = "text-white float-left ml-5 mt-10 text-balance">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque earum eius ex excepturi facilis incidunt iusto laborum laudantium magnam nisi optio, pariatur provident qui quo rerum sit temporibus ullam voluptatum.
+
+            </h2>
+
+
         </div>
+
+
     )
 }
 export default Guide;

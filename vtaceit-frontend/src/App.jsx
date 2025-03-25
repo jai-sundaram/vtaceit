11b14c12e7584bbeaa9e8ab6e983614b537d8e7a
jaivatsan-elango-sundaram <je.sundaram@gmail.com>
2025-03-25 00:32:54 -0400

    connected to backend

--- vtaceit-frontend/src/App.jsx
+++ vtaceit-frontend/src/App.jsx
@@ -1,13 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useEffect, useState} from 'react'
+
 import './App.css'
 import Guide from "./assets/Guide.jsx";
+import axios from "axios";
 
 function App() {
+  const [guides, setGuides] = useState([]);
 
+  useEffect(()=>{
+    const getData = async() =>{
+      try{
+        const response = await axios.get("http://localhost:8080/allGuides");
+        setGuides(response.data);
+        console.log(response.data);
+      }
+      catch(err){
+        console.log(err);
+      }
+    };
+    getData();
+  }, [])
   return (
-      <Guide />
+      <ul className = "flex flex-col gap-20 justify-center max-w-screen">
+        {guides.map((guide)=>(
+            <Guide guide = {guide} />
+            ))}
+        <br />
+      </ul>
   )
 }
 
--- vtaceit-frontend/src/assets/Guide.jsx
+++ vtaceit-frontend/src/assets/Guide.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 
-const Guide = () => {
+const Guide = ({guide}) => {
+
     return (
         <div className="border-2 w-250 h-100 bg-vtorange ">
             <h1 className = "mt-5 mr-219 mt-">DIFFICULTY</h1>
 
             <div className="mt-1">
-                <h2 className = "text-white float-left ml-35  text-lg">Professor: Juste</h2>
-                <h2 className= "text-white float-right mr-6 text-lg "> Date Posted: 8/20/2024</h2>
+                <h2 className = "text-white float-left ml-35  text-lg">Professor: {guide.profName}</h2>
+                <h2 className= "text-white float-right pr-5 text-lg "> Date Posted: {guide.date[1]}/{guide.date[2]}/{guide.date[0]}</h2>
             </div>
-            <div className="text-5xl text-center ml-5 border-1 border-black w-20 h-20 bg-gray-500 ">4</div>
+            <div className="text-5xl text-center ml-5 border-1 border-black w-20 h-20 bg-gray-500 ">{guide.difficulty}</div>
             <div className = "">
-                <h2 className = "text-white float-left ml-35 mt-1">Grade: B+</h2>
-                <h2 className = "text-white float-left ml-40 mt-1">Semester: Spring 2024</h2>
-                <h2 className = "text-white float-left ml-40 mt-1">Attendance: Mandatory</h2>
+                <h2 className = "text-white float-left ml-35 mt-1">Grade: {guide.grade}</h2>
+                <h2 className = "text-white float-left ml-40 mt-1">Semester: {guide.semTaken}</h2>
+                <h2 className = "text-white float-left ml-40 mt-1">Attendance: {guide.attendanceReq}</h2>
             </div>
-            <h2 className = "text-white float-left ml-5 mt-10 text-balance">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque earum eius ex excepturi facilis incidunt iusto laborum laudantium magnam nisi optio, pariatur provident qui quo rerum sit temporibus ullam voluptatum.
+            <h2 className = "text-white mr-150  mt-10 ">
+                {guide.comments}
 
             </h2>
 
@@ -25,5 +26,5 @@ const Guide = () => {
 
 
     )
-}
+ }
 export default Guide;
